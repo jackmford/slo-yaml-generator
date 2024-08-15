6e2eeaec96ffec7ffd84d9a71d3336c23f267503
jackmford <jackmitchellfordyce@gmail.com>
2024-08-15 18:57:04 -0500

    feat: decided to change course
    Now writing in OpenSLO, validating, and converting to N9

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import argparse
 import logging
+import os
 
 from jinja2 import Template
 
 
 logger = logging.getLogger(__name__)
 
+def make_service(args):
+    with open("./templates/openslo-service.yaml.j2", "r") as file:
+        yaml_template = file.read()
+
+    template = Template(yaml_template)
+    values = {
+        "service_name": args.resource_name
+    }
+
+    if args.description:
+        values["description"] = args.description
+
+    processed_service = template.render(values)
+    with open("./templates/tmp-service.yaml", "w") as file:
+        file.write(processed_service)
+        file.close()
+    out = os.system("oslo validate -f ./templates/tmp-service.yaml")
+    print(out)
+    if out == 0:
+        print("yay")
+        out = os.system("oslo convert -f ./templates/tmp-service.yaml -o nobl9 > tmp-service.yaml")
+    return
+
+
 def make_project(project_name):
-    with open("./project.yaml.j2", "r") as file:
+    with open("./templates/project.yaml.j2", "r") as file:
         yaml_template = file.read()
 
     template = Template(yaml_template)
@@ -16,20 +41,24 @@ def make_project(project_name):
     }
 
     processed_project = template.render(values)
+    # Need to write tmp file, push it to n9, and delete it
     print(processed_project)
     return
 
 def main():
     parser = argparse.ArgumentParser(description='Process Nobl9 yaml inputs.')
+    parser.add_argument("--project_name", help="Project Name", required=True)
     parser.add_argument("--resource_type", help="Project, SLO, Service, Integration", required=True)
-    parser.add_argument("--project_name", help="Project name", required=True)
+    parser.add_argument("--resource_name", help="Resource name", required=True)
+    parser.add_argument("--description", help="Description of resource")
     args = parser.parse_args()
 
-    logger.propagate
     print(args)
     print(args.resource_type)
     if args.resource_type.lower() == "project":
-        make_project(args.project_name)
+        make_project(args)
+    elif args.resource_type.lower() == "service":
+        make_service(args)
     logger.debug(args)
     return
 
